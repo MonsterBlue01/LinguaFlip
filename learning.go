602f8788a00d3a8fd580beaf7052b60961213669
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-07-19 20:36:41 -0700

    2022/07/19 20:30

---
+++ learning.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* This is my first simple program */
+    fmt.Println("Hello, World!")
+	fmt.Println("Google" + "Runoob")					// concatenation of two strings
+
+	var stockcode=123
+    var enddate="2020-12-31"
+    var url="Code=%d&endDate=%s"						// format string
+    var target_url=fmt.Sprintf(url,stockcode,enddate)
+    fmt.Println(target_url)
+}
+
+// single line comment
+/*
+	I am a multiline comment
+ */
