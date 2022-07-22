dd99b235f206ba13a188ba6a907cda6d35c2c854
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-07-22 13:14:35 -0700

    2022/07/22 13:15

--- Go learning/learning.go
+++ Go learning/learning.go
@@ -38,7 +38,7 @@ func main() {
     var h string
     fmt.Printf("%v %v %v %q\n", i, f, g, h)
 
-    j := 1                                              // This can be used as a way of declaring variables
+    // j := 1                                           // This can be used as a way of declaring variables
 
     var k = true
     fmt.Println(k)                                      // In this case the system automatically classifies the variable
@@ -46,9 +46,9 @@ func main() {
     // var vname1, vname2, vname3 type
     // vname1, vname2, vname3 = v1, v2, v3
 
-    // var vname1, vname2, vname3 = v1, v2, v3 // In this case the system automatically classifies the variable
+    // var vname1, vname2, vname3 = v1, v2, v3          // In this case the system automatically classifies the variable
 
-    // vname1, vname2, vname3 := v1, v2, v3 // Variables appearing on the left-hand side of := should not have already been declared, otherwise a compilation error will result
+    // vname1, vname2, vname3 := v1, v2, v3             // Variables appearing on the left-hand side of := should not have already been declared, otherwise a compilation error will result
 
 
     // // This way of writing the factorization keyword is generally used to declare global variables
@@ -56,6 +56,29 @@ func main() {
     //     vname1 v_type1
     //     vname2 v_type2
     // )
+
+    const LENGTH int = 10
+    const WIDTH int = 5                                 // Two examples of const variables
+    var area int
+    const l, m, n = 1, false, "str"                     // multiple assignment
+
+    area = LENGTH * WIDTH
+    fmt.Printf("The area is : %d", area)
+    println()
+    println(l, m, n)
+
+    const (                                             // An example that use "iota" in "enumerate"
+        a = iota                                        // 0
+        b                                               // 1
+        c                                               // 2
+        d = "ha"                                        // independent value, iota += 1
+        e                                               // "ha"   iota += 1
+        f = 100                                         // iota +=1
+        g                                               // 100  iota +=1
+        h = iota                                        // 7, restore count
+        i                                               // 8
+    )
+    fmt.Println(a, b, c, d, e, f, g, h, i)
 
 }
 
