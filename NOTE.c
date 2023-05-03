044b5a9109820309085cb0280d7fcda4ac312646
monstblu <monstblu@kali.kali>
2023-05-02 18:29:32 -0700

    05/02/2023 6:30 PM

--- NOTE.c
+++ NOTE.c
@@ -1,1 +1,51 @@
-Cargue el archivo CSV en Django primero y luego manipúlelo.
+// Current problem:
+// 1. I need the datatype of all columns
+// 2. If it does not match, can it be directly replaced with None?
+// 3. What does no entry mean?
+// 4. I don't fully understand what the second requirement means '
+
+// Code changed so far:
+// import csv
+
+// # ... (other functions remain the same)
+
+// # Define the expected data types for each column
+// EXPECTED_DATA_TYPES = {
+//     'column1': int,
+//     'column2': float,
+//     'column3': str,
+//     # Add more columns as needed
+// }
+
+// def is_valid_data_type(value, data_type):
+//     try:
+//         data_type(value)
+//         return True
+//     except ValueError:
+//         return False
+
+// # Modified remove_columns function
+// def remove_columns(input_file, output_file, columns_to_remove):
+//     with open(input_file, 'r', newline='', encoding='iso-8859-1') as infile, open(output_file, 'w', newline='') as outfile:
+//         # Read the input file as plain text
+//         lines = infile.readlines()
+
+//         # Process the header row
+//         header = lines[0].strip().split(',')
+//         new_header = [header[i] for i in range(len(header)) if i not in columns_to_remove]
+//         outfile.write(','.join(new_header) + '\n')
+
+//         # Process the remaining rows
+//         for line in lines[1:]:
+//             row = line.strip().split(',')
+//             new_row = []
+//             for i in range(len(row)):
+//                 if i not in columns_to_remove:
+//                     column_name = header[i]
+//                     if column_name in EXPECTED_DATA_TYPES and not is_valid_data_type(row[i], EXPECTED_DATA_TYPES[column_name]):
+//                         new_row.append('None')
+//                     else:
+//                         new_row.append(row[i])
+//             outfile.write(','.join(new_row) + '\n')
+
+// # ... (remaining code remains the same)
