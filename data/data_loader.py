b73e60e19d8060bf293a454d5062bb91b02b5e02
MonsterBlue01 <monstblu@proton.me>
2024-12-28 15:55:03 -0800

    Update data crawling and preliminary cleaning logic
    - Enhanced logic for data crawling from ParaCrawl.
    - Improved preliminary cleaning steps, including filtering by length and content.
    - Observed that ParaCrawl data quality is relatively high; file size remains largely unchanged.

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,27 +1,19 @@
 from datasets import load_dataset
 import os
 
-def clean_and_save_streaming(output_file, chuck_size = 10000):
+def clean_and_save_streaming(output_file):
     dataset = load_dataset("para_crawl", "enes", split="train", streaming=True)
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
     with open(output_file, "w") as f:
         f.write("en,es\n")
 
-    cleaned_chuck = []
-    for i, example in enumerate(dataset):
-        src, tgt = example["translation"]["en"], example["translation"]["es"]
-        if 3 <= len(src.split()) <= 128 and 3 <= len(tgt.split()) <= 128:
-            cleaned_chuck.append(f"{src},{tgt}\n")
-
-        if (i + 1) % chuck_size == 0:
-            with open(output_file, "a") as f:
-                f.writelines(cleaned_chuck)
-            cleaned_chuck = []
-
-    if cleaned_chuck:
-        with open(output_file, "a") as f:
-            f.writelines(cleaned_chuck)
+        for i, example in enumerate(dataset):
+            src, tgt = example["translation"]["en"], example["translation"]["es"]
+            if (3 <= len(src.split()) <= 128 and 3 <= len(tgt.split()) <= 128 and
+                "http" not in src and "www." not in src and
+                "http" not in tgt and "www." not in tgt):
+                f.write(f"{src},{tgt}\n")
 
     print(f"Cleaned data saved to {output_file}")
 
